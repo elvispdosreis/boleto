5d0f7ee14751f11d5e6bdcdd509de187ceb7325b
Elvis P dos Reis <elvisdosreis@gmail.com>
2019-10-30 16:02:14 -0300

    Update BrasilService.php

--- src/Bank/BrasilService.php
+++ src/Bank/BrasilService.php
@@ -416,9 +416,16 @@ class BrasilService implements InterfaceBank
             $context = stream_context_create($httpHeaders);
 
             if ($this->isSandbox()) {
+                /* Problema de SSL no Endpoint
                 $endpoint = 'https://cobranca.homologa.bb.com.br:7101/Processos/Ws/RegistroCobrancaService.serviceagent?wsdl';
+                */
+                $endpoint = 'https://cobranca.homologa.bb.com.br:7101/Processos/Ws/RegistroCobrancaService.serviceagent?wsdl';
+                $endpoint = dirname(__FILE__) . '/../XSD/Banco do Brasil/RegistroCobrancaServiceHomologacao.xml';
             } else {
+                /* Problema de SSL no Endpoint
                 $endpoint = 'https://cobranca.bb.com.br:7101/Processos/Ws/RegistroCobrancaService.serviceagent?wsdl';
+                */
+                $endpoint = dirname(__FILE__) . '/../XSD/Banco do Brasil/RegistroCobrancaService.xml';
             }
 
             $client = new SoapClient($endpoint,
